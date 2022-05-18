2259d2c2734eea484829450f302d018df0c12d27
Md. Mozahidul Islam Bhuiyan <mozahidmozahidul@gmail.com>
2022-05-18 22:06:51 +0600

    Add files via upload

---
+++ Design Patterns/Factory Method Design Pattern/Example 01/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Factory_Method_Pattern
+{
+
+    public abstract class Logistics
+    {
+        public abstract ITransport GetTransport();
+
+        public void Transport()
+        {
+            ITransport transport = GetTransport();
+            transport.Deliver();
+        }
+    }
+
+    public class RoadLogistics: Logistics
+    {
+        public override ITransport GetTransport(){
+            return new Truck();
+        }
+    }
+
+    public class ShipLogistics: Logistics
+    {
+        public override ITransport GetTransport(){
+            return new Ship();
+        }
+    }
+
+    public interface ITransport
+    {
+        void Deliver();
+    }
+
+    class Truck : ITransport
+    {
+        public void Deliver()
+        {
+            Console.WriteLine("Delivered by Truck");
+        }
+    }
+
+    class Ship : ITransport
+    {
+        public void Deliver()
+        {
+            Console.WriteLine("Delivered by Ship");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientCode(new RoadLogistics());
+            ClientCode(new ShipLogistics());
+        }
+
+        static void ClientCode(Logistics logistics)
+        {
+            logistics.Transport();
+        }
+    }
+}
