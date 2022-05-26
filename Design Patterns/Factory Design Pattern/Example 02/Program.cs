9eb231d3b5fd74be934c5b9efaa29902e0377370
Md. Mozahidul Islam Bhuiyan <mozahidmozahidul@gmail.com>
2022-05-26 11:39:37 +0600

    Add files via upload

---
+++ Design Patterns/Factory Design Pattern/Example 02/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    public interface CreditCard
+    {
+        string GetCardType();
+        int GetCreditLimit();
+        int GetAnnualCharge();
+    }
+
+    class MoneyBack: CreditCard
+    {
+        public string GetCardType(){
+            return "MoneyBack";
+        }
+
+        public int GetCreditLimit(){
+            return 100000;
+        }
+
+        public int GetAnnualCharge(){
+            return 500;
+        }
+    }
+
+    class Titanium : CreditCard
+    {
+        public string GetCardType(){
+            return "Titanium";
+        }
+
+        public int GetCreditLimit(){
+            return 1000000;
+        }
+
+        public int GetAnnualCharge(){
+            return 800;
+        }
+    }
+
+    class Platinum : CreditCard
+    {
+        public string GetCardType(){
+            return "Platinum";
+        }
+
+        public int GetCreditLimit(){
+            return 10000000;
+        }
+
+        public int GetAnnualCharge(){
+            return 1000;
+        }
+    }
+
+
+    class CreditCardFactory
+    {
+        public static CreditCard GetCreditCard(string cardType)
+        {
+            switch(cardType)
+            {
+                case "MoneyBack":
+                    return new MoneyBack();
+                    break;
+                
+                case "Titanium":
+                    return new Titanium();
+                    break;
+
+                case "Platinum":
+                    return new Platinum();
+                    break;
+                default:
+                    return null;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCard card = CreditCardFactory.GetCreditCard("MoneyBack");
+            if(card != null){
+                Console.WriteLine(card.GetCardType());
+            }
+        }
+    }
+}
